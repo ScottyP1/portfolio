c96ded95574507359c000087ad080a1b344daa74
Cody Scott <scotty_p@Codys-MacBook-Pro.local>
2024-08-13 21:29:41 -0400

    update urls

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -14,7 +14,7 @@ export default function Projects() {
                 <VerticalCard
                     image={SweetStingImg}
                     imageAlt='Sweet Sting ScreenShot'
-                    link="https://sweet-sting-kqka8yokm-cody-scotts-projects.vercel.app/"
+                    link="https://sweet-sting.vercel.app/"
                     title='Sweet Sting'
                     description="Developed a custom website for a piercing company using React, focused on providing a modern, user-friendly experience. The site
                 features a clean and aesthetic design, showcasing the company's services, portfolio, and customer testimonials. Key functionalities include an 
@@ -23,7 +23,7 @@ export default function Projects() {
                 <VerticalCard
                     image={NetflixImg}
                     imageAlt='Netflix Clone ScreenShot'
-                    link="https://next-app-ias5-h6xpn9qkq-cody-scotts-projects.vercel.app/"
+                    link="https://next-app-ias5.vercel.app/"
                     title='Netflix Clone'
                     description='A Netflix clone built with React and Next.js, replicating the core functionality and user interface of the popular streaming platform.
                 The project includes features such as user authentication, movie and TV show browsing, a responsive design, and dynamic content loading.'/>
@@ -31,7 +31,7 @@ export default function Projects() {
                 <VerticalCard
                     image={ThorImg}
                     imageAlt='Thor Transport ScreenShot'
-                    link="https://thor-mg510vppm-cody-scotts-projects.vercel.app/"
+                    link="https://thor-two.vercel.app/"
                     title='Thor Transport'
                     description='Created a website for a semi truck company specializing in road freight, leveraging React for a dynamic and responsive user experience.
                      The site features detailed service descriptions, a fleet overview, and an easy-to-use booking system for freight services.' />
